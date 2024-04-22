1b27e90bb9df97d21ed278a29bd54e99da40a4c4
mateocapon <mcapon@fi.uba.ar>
2024-04-22 14:12:16 -0300

    change main

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tinderlibs',
     version='0.1.1',
-    py_modules=['tinderlibs']
+    packages=find_packages(),
+    author='Tinder - Taller3',
+    author_email='mcapon@fi.uba.ar',
+    description='Paquetes common al backend de Taller',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/taller3-tinder/tinderlibs',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
