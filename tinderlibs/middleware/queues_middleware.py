5bb21ac613661fffdc8f58aac35ba2b87a234768
mateocapon <mcapon@fi.uba.ar>
2024-04-24 18:08:03 -0300

    sns and sqs clients

--- tinderlibs/middleware/queues_middleware.py
+++ tinderlibs/middleware/queues_middleware.py
@@ -8,28 +8,35 @@ AWS_REGION = os.getenv("AWS_REGION")          # Ex: 'us-east-1'
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")  # Ex: 'dummy'
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")  # Ex: 'dummy'
 QUEUE_NAME = os.getenv("QUEUE_NAME")  # Ex: my-queue
-AWS_CLIENT = os.getenv("AWS_CLIENT")  # Ex: aws-localstack
 TOPIC_PUBLISH = os.getenv("SNS_TOPIC")  # Ex: my-topic
 
 
 class QueuesMiddleware:
     def __init__(self):
-        self.aws_client = boto3.client(AWS_CLIENT,
-                                       endpoint_url=MIDDLEWARE_HOST,
-                                       region_name=AWS_REGION,
-                                       aws_access_key_id=AWS_ACCESS_KEY,
-                                       aws_secret_access_key=AWS_SECRET_KEY)
+        self.sqs = boto3.client('sqs',
+                                endpoint_url=MIDDLEWARE_HOST,
+                                region_name=AWS_REGION,
+                                aws_access_key_id=AWS_ACCESS_KEY,
+                                aws_secret_access_key=AWS_SECRET_KEY)
+
+        if TOPIC_PUBLISH:
+            self.sns = boto3.client('sns',
+                                    endpoint_url=MIDDLEWARE_HOST,
+                                    region_name=AWS_REGION,
+                                    aws_access_key_id=AWS_ACCESS_KEY,
+                                    aws_secret_access_key=AWS_SECRET_KEY)
+
         self.listening = False
         if QUEUE_NAME:
             self.queue_url = self.__get_queue_url(QUEUE_NAME)
 
     def __get_queue_url(self, queue_name):
-        return self.aws_client.get_queue_url(QueueName=queue_name)['QueueUrl']
+        return self.sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
 
     def start_receiving(self, callback):
         self.listening = True
         while (self.listening):
-            messages = self.aws_client.receive_message(
+            messages = self.sqs.receive_message(
                 QueueUrl=self.queue_url,
                 MaxNumberOfMessages=10,
                 WaitTimeSeconds=20
@@ -37,18 +44,21 @@ class QueuesMiddleware:
             if 'Messages' in messages:
                 for message in messages['Messages']:
                     callback(message['Body'])
-                    self.aws_client.delete_message(
+                    self.sqs.delete_message(
                         QueueUrl=self.queue_url,
                         ReceiptHandle=message['ReceiptHandle'])
 
+    def stop(self):
+        self.listening = False
+
     def send(self, message):
-        self.aws_client.send_message(
+        self.sqs.send_message(
             QueueUrl=self.queue_url,
             MessageBody=message
         )
 
     def publish(self, message):
-        self.aws_client.publish(
+        self.sns.publish(
             TopicArn=TOPIC_PUBLISH,
             Message=message
         )
